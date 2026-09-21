a676426ec89b4b8cb50390317b37aacb2a3805c4
Lucas Daniel <militao.lucas@escola.pr.gov.br>
2026-09-21 09:56:04 -0300

    arrumando o direcionamento

--- server.js
+++ server.js
@@ -5,21 +5,24 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+});
+
 app.post('/api/login', (req, res) => {
     const { tipo, login, senha } = req.body;
-    
+
     if (tipo === 'profissional') {
         const usuariosValidos = ['lucas daniel', 'gabriel tavares'];
-        
-        // Verifica se o usuário digitado está na lista e se a senha confere
+
         if (usuariosValidos.includes(login.toLowerCase()) && senha === '321123') {
-            return res.json({ sucesso: true, redirecionar: '/gestao.html' });
+            return res.json({ sucesso: true, redirecionar: '/html/gestao.html' });
         }
         return res.status(401).json({ sucesso: false, mensagem: "Usuário ou senha incorretos." });
     }
 
     if (tipo === 'responsavel' && senha) {
-        return res.json({ sucesso: true, redirecionar: '/gestao.html' });
+        return res.json({ sucesso: true, redirecionar: '/html/gestao.html' });
     }
 
     res.status(401).json({ sucesso: false, mensagem: "Acesso negado." });
